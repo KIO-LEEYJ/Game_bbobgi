528e1303257639bb1ff5e5c6a4d1c6fa5afba1f7
KIO <creator.leeyj@gmail.com>
2025-04-15 09:56:52 +0900

    2025-04-15 09:56 🔄 Auto commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
